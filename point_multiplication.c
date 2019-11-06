3f5605bcd15e53ecc3ffea198e9bfddc00937ac1
Gustavo Schwantz <gustavo_aso1994@hotmail.com>
2019-11-05 21:47:20 -0300

    Algoritmo Rho de Pollard, pronto!

--- point_multiplication.c
+++ point_multiplication.c
@@ -6,15 +6,30 @@
 
 void double_and_add (struct Point *T, const struct Point P, mpz_t d, const struct Curve c)
 {
+	if ( mpz_cmp_si (d, 0) == 0 ) {
+    	mpz_set_str (T->x, "0", 10);
+	    mpz_set_str (T->y, "0", 10);
+	    mpz_set (T->k, c.n);
+
+	    return;
+    }
+
     mpz_set (T->x, P.x);
     mpz_set (T->y, P.y);
+    mpz_set (T->k, P.k);
 
     int i;
+
+    // gmp_printf ("T = (%Zd, %Zd), k = %Zd\n", T->x, T->y, T->k);
 
     for (i = (mpz_sizeinbase (d, 2) - 2) ; i >= 0 ; --i) {
         point_operation (T, *T, *T, c);
 
-        if (mpz_tstbit (d, i) == 1) 
-            point_operation (T, P, *T, c);
+        // gmp_printf ("T depois do Doubling = (%Zd, %Zd), k = %Zd\n", T->x, T->y, T->k);
+
+        if (mpz_tstbit (d, i) == 1) {
+            point_operation (T, *T, P, c);
+            // gmp_printf ("T depois da Addition = (%Zd, %Zd), k = %Zd\n", T->x, T->y, T->k);
+        }
     }
 }
--- rho_of_pollard.c
+++ rho_of_pollard.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <gmp.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include "curve.h"
 #include "modular_arithmetic.h"
@@ -28,26 +31,27 @@ void rho_of_pollard (mpz_t d, const struct Point P, const struct Point Q,
 int main(int argc, char const *argv[])
 {
 	struct Curve c;
-	struct Point P, Q;
-	mpz_t d, n, a, b;
+	struct Point P, Q, R;
+	mpz_t d, n;
 
-	curve_init (&c); 
+	mpz_init_set_str (n, "239", 10);
+
+	curve_init (&c, n); 
  	point_init (&P);  
-	point_init (&Q); 
+	point_init (&Q);
+	point_init (&R);
 	mpz_init (d);
-	mpz_init (n);
     
-	mpz_set_str (c.a, "2", 10);
-	mpz_set_str (c.b, "2", 10);
-	mpz_set_str (c.p, "17", 10);
+	mpz_set_str (c.a, "1", 10);
+	mpz_set_str (c.b, "44", 10);
+	mpz_set_str (c.p, "229", 10);
     
 	mpz_set_str (P.x, "5", 10);
-	mpz_set_str (P.y, "1", 10);
+	mpz_set_str (P.y, "116", 10);
 
-	mpz_set_str (n, "19", 10);
-
-	mpz_set_str (Q.x, "5", 10);
-	mpz_set_str (Q.y, "16", 10);
+	mpz_set_str (Q.x, "155", 10);
+	mpz_set_str (Q.y, "166", 10);
+	mpz_set_str (Q.k, "176", 10);
 
 	rho_of_pollard (d, P, Q, n, c);
 
@@ -56,6 +60,7 @@ int main(int argc, char const *argv[])
 	curve_clear (&c);
 	point_clear (&P);
 	point_clear (&Q);
+	point_clear (&R);
 	mpz_clear (d);
 	mpz_clear (n);
 
@@ -100,13 +105,28 @@ int H (const struct Point P)
 void set_random (mpz_t a, mpz_t b, const mpz_t n)
 {
 	gmp_randstate_t state;
+	
+	gmp_randinit_mt (state);
 
-	gmp_randinit_default (state);
+	// início código StackOverflow
+    int fp = open("/dev/random", O_RDONLY);
+    if (fp == -1) abort();
+    unsigned seed;
+    unsigned pos = 0;
+    while (pos < sizeof(seed)) {
+        int amt = read(fp, (char *) &seed + pos, sizeof(seed) - pos);
+        if (amt <= 0) abort();
+        pos += amt;
+    }
+    close(fp);
+	// fim código StackOverflow
 
-	gmp_randseed_ui (state, time (0));
+	gmp_randseed_ui (state, seed);
 
 	mpz_urandomm (a, state, n);
 	mpz_urandomm (b, state, n);
+
+	gmp_randclear (state);
 }
 
 void mul_and_add (struct Point *R, mpz_t a, mpz_t b, const struct Point P, 
@@ -144,44 +164,13 @@ void rho_of_pollard (mpz_t d, const struct Point P, const struct Point Q,
 	for (j = 0; j < L; ++j) {
 		set_random (table[j].a, table[j].b, n);
         mul_and_add (&(table[j].P), table[j].a, table[j].b, P, Q, c);
-        gmp_printf ("aj = %Zd ", table[j].a);
-        gmp_printf ("bj = %Zd ", table[j].b);
-        gmp_printf ("Rj = (%Zd, %Zd)\n\n", table[j].P.x, table[j].P.y);
 	}
 
-	//mpz_set_str (table[0].a, "79", 10);
-	//mpz_set_str (table[0].b, "163", 10);
-	//mpz_set_str (table[0].P.x, "135", 10);
-	//mpz_set_str (table[0].P.y, "117", 10);
-	//mpz_set_str (table[1].a, "206", 10);
-	//mpz_set_str (table[1].b, "19", 10);
-	//mpz_set_str (table[1].P.x, "96", 10);
-	//mpz_set_str (table[1].P.y, "97", 10);
-	//mpz_set_str (table[2].a, "87", 10);
-	//mpz_set_str (table[2].b, "109", 10);
-	//mpz_set_str (table[2].P.x, "84", 10);
-	//mpz_set_str (table[2].P.y, "62", 10);
-	//mpz_set_str (table[3].a, "219", 10);
-	//mpz_set_str (table[3].b, "68", 10);
-	//mpz_set_str (table[3].P.x, "72", 10);
-	//mpz_set_str (table[3].P.y, "134", 10);
-
     set_random (c1, d1, n);
-    //mpz_set_str (c1, "54", 10);
-	//mpz_set_str (d1, "175", 10);
     mul_and_add (&X1, c1, d1, P, Q, c);
 
-    gmp_printf ("c1 = %Zd ", c1);
-    gmp_printf ("d1 = %Zd ", d1);
-    gmp_printf ("X1 = (%Zd, %Zd) ", X1.x, X1.y);
-
-    mpz_set (X2.x, X1.x); mpz_set (X2.y, X1.y); mpz_set (c2, c1); mpz_set (d2, d1);
-
-    gmp_printf ("c2 = %Zd ", c2);
-    gmp_printf ("d2 = %Zd ", d2);
-    gmp_printf ("X2 = (%Zd, %Zd)\n", X2.x, X2.y);
-    
-    int x = 1;
+    mpz_set (X2.x, X1.x); mpz_set (X2.y, X1.y); mpz_set (X2.k, X1.k); 
+    mpz_set (c2, c1); mpz_set (d2, d1);
 
     do {
 
@@ -191,40 +180,15 @@ void rho_of_pollard (mpz_t d, const struct Point P, const struct Point Q,
     	mod_add (c1, c1, table[j].a, n);
     	mod_add (d1, d1, table[j].b, n);
 
-    	printf("x = %d ", x++);
-
-        gmp_printf ("c1 = %Zd ", c1);
-        gmp_printf ("d1 = %Zd ", d1);
-        gmp_printf ("X1 = (%Zd, %Zd)\n", X1.x, X1.y);
-
     	for (i = 0; i < 2; ++i) {
     		j = H (X2); 
 
     	    point_operation (&X2, X2, table[j].P, c);
     	    mod_add (c2, c2, table[j].a, n);
     	    mod_add (d2, d2, table[j].b, n);
-
-    	    printf("i = %d ", i);
-
-            gmp_printf ("c2 = %Zd ", c2);
-            gmp_printf ("d2 = %Zd ", d2);
-            gmp_printf ("X2 = (%Zd, %Zd)\n", X2.x, X2.y);
     	}
-        
-        // printf("x = %d ", x++);
-
-        // gmp_printf ("c1 = %Zd ", c1);
-        // gmp_printf ("d1 = %Zd ", d1);
-        // gmp_printf ("X1 = (%Zd, %Zd) ", X1.x, X1.y);
-
-        // gmp_printf ("c2 = %Zd ", c2);
-        // gmp_printf ("d2 = %Zd ", d2);
-        // gmp_printf ("X2 = (%Zd, %Zd)\n", X2.x, X2.y);
 
     } while ( !(mpz_cmp (X1.x, X2.x) == 0 && mpz_cmp (X1.y, X2.y) == 0) );
-
-    // gmp_printf ("X1 = (%Zd, %Zd)\n", X1.x, X1.y);
-    // gmp_printf ("X2 = (%Zd, %Zd)\n", X2.x, X2.y);
     
     if ( (mpz_cmp (d1, d2) == 0 ) )
     	printf("failure\n");
